e3677e8db486464bcb402d1179a80619f32e839e
unknown <rdethlefs850@wit-xdvm-017.MILLARDPS.NET>
2013-01-31 12:16:42 -0600

    Initial commit

---
+++ layout/nav.php
@@ -0,0 +1,9 @@
+<div class="navbar">
+	<div class="navbar-inner">
+		<a class="brand" href="./">My Teams</a>	
+		<ul class="nav">
+			<li><a href="./?p=list_teams">Teams</a></li>
+			<li><a href="./?p=form_add_team">Add Team</a></li>
+		</ul>
+	</div>
+</div>
